37fbb93fc1da35e5c5e52799179f5990e365ba10
PabloSierra <13sierras@gmail.com>
2023-10-09 21:36:55 +0200

    Update

--- Repaso_Programacion1_PabloSierra/src/main/java/Ej1/MainTrabajador.java
+++ Repaso_Programacion1_PabloSierra/src/main/java/Ej1/MainTrabajador.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.repaso_programacion1.EJ1;
+package Ej1;
 
 /**
  *
